d1a11be523d59bd4ce22e581986ae1432e839dab
Chike Chris <cchike2017@gmail.com>
2019-06-19 15:18:39 -0400

    workin on the forEach() to iterate over the article

--- Article/Article.js
+++ Article/Article.js
@@ -10,19 +10,24 @@ class Article {
     this.expandButton.textContent = "expand";
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener("click", () => {
+      this.expandArticle();
+    });
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.classList.toggle("article-open");
   }
 }
 
-/* START HERE: 
+//START HERE:
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+//Select all classes named ".article" and assign that value to the articles variable.
+let articles = document.querySelectorAll(".article");
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
+//With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
-*/
-
-let articles;
+articles = Array.from(articles).forEach(article => {
+  return new Article(article);
+});
