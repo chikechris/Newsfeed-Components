ebdccdac9e079f30aac4c84a0e255975cbbeec5f
Chike Chris <cchike2017@gmail.com>
2019-07-10 15:28:51 -0400

    working on the articles.js file

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,29 @@
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle("menu--open");
-};
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu refence.
+//   menu.classList.toggle("menu--open");
+// };
 
-// Start Here: Create a reference to the ".menu" class
+// // Start Here: Create a reference to the ".menu" class
+// const menu = document.querySelector(".menu");
+// // create a reference to the ".menu-button" class
+// const menuButton = document.querySelector(".menu-button");
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+// menuButton.addEventListener("click", toggleMenu);
+
+{
+  /* <div class="menu">
+  <ul>
+    <li>Students</li>
+    <li>Faculty</li>
+    <li>What's New</li>
+    <li>Tech Trends</li>
+    <li>Music</li>
+    <li>Log Out</li>
+  </ul>
+</div> */
+}
+
 const menu = document.querySelector(".menu");
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button");
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", toggleMenu);
+
+const uList = document.createElement("ul");
+const chList = document.createElement("li");
